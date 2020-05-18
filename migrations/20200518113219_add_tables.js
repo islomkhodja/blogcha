16b4a534ea899041904eb7abc12e5dcdca8bc11b
Islomkhodja <hamidullakhodjaev@gmail.com>
2020-05-18 15:09:21 +0500

    fix migration: created time default is now

--- migrations/20200518113219_add_tables.js
+++ migrations/20200518113219_add_tables.js
@@ -4,14 +4,14 @@ exports.up = async function(knex) {
         table.string('name');
         table.string('email');
         table.text('bio');
-        table.dateTime('created_at');
+        table.dateTime('created_at').defaultTo(knex.fn.now());
         table.boolean('is_deleted');
     })
 
     await knex.schema.createTable('categories', (table) => {
         table.increments('id')
         table.string('title');
-        table.dateTime('created_at');
+        table.dateTime('created_at').defaultTo(knex.fn.now());
         table.integer('created_by').references('id').inTable('users');
         table.boolean('is_deleted');
     })
@@ -21,7 +21,7 @@ exports.up = async function(knex) {
         table.string('title');
         table.text('body');
         table.integer('category_id').references('id').inTable('categories');
-        table.dateTime('created_at');
+        table.dateTime('created_at').defaultTo(knex.fn.now());
         table.integer('created_by').references('id').inTable('users');
         table.boolean('is_deleted');
     })
