56a80068ef595fefbe1c8d843792089f78540749
Islomkhodja <hamidullakhodjaev@gmail.com>
2020-05-19 14:43:03 +0500

    posts and users index.js

---
+++ src/features/posts/index.js
@@ -0,0 +1,8 @@
+const { PostsRepo } = require('./posts.repo');
+const { Posts } = require('./posts.model');
+const PostsRouter = require('./posts.route');
+module.exports = {
+    PostsRepo,
+    Posts,
+    PostsRouter,
+}
---
+++ src/features/users/index.js
@@ -0,0 +1,8 @@
+const { UsersRepo } = require('./users.repo');
+const { Users } = require('./users.model');
+const UsersRouter = require('./users.route');
+module.exports = {
+    UsersRepo,
+    Users,
+    UsersRouter,
+}
