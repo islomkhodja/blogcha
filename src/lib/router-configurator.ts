c3e3ac5fb8edd7464ae74620a240867d20d4c711
Islomkhodja <hamidullakhodjaev@gmail.com>
2022-07-22 17:24:51 +0500

    feature: router configurator for controller

---
+++ src/lib/router-configurator.ts
@@ -0,0 +1,40 @@
+import {
+  AppController,
+  ApplicationRouterSettings,
+  RouteHandler,
+} from "../modules/categories/categories.controllers";
+import { Router } from "express";
+
+const setToRoute = <T>(route, router, handlerObject, controller: T) => {
+  const { method, beforeMiddleware, afterMiddleware, handler } = handlerObject;
+  router[method.toLowerCase()](
+    route,
+    ...beforeMiddleware,
+    async (req, res, next) => {
+      try {
+        const boundHandler = handler.bind(controller);
+        return await boundHandler(req, res, next);
+      } catch (err) {
+        next(err);
+      }
+    },
+    ...afterMiddleware
+  );
+};
+
+export const routerConfigurationForController = <T extends AppController>(
+  controller: T,
+  router: Router
+) => {
+  const routerSettings = controller.routerSettings();
+  Object.keys(routerSettings).forEach((path) => {
+    const routeHandlers = routerSettings[path];
+    if (Array.isArray(routeHandlers)) {
+      for (let routeHandlerObject of routeHandlers as RouteHandler[]) {
+        setToRoute(path, router, routeHandlerObject, controller);
+      }
+    } else {
+      setToRoute(path, router, routeHandlers, controller);
+    }
+  });
+};
